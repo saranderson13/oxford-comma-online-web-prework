e32291fd67864f60f65865493c9ca39e9522ffa5
Sarah Anderson <saranderson13@gmail.com>
2019-03-19 16:17:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case
+    when array.length == 1
+      array.join
+    when array.length == 2
+      array.join(" and ")
+    when array.length >= 3
+      last_item = array.pop
+      string_list = array.join(", ")
+      string_list << ", and #{last_item}"
+      string_list
+  end
 end
