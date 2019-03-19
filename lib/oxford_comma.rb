6279c39fe08b0fa5d5ee5477adc34bfb9b7611c5
Sarah Anderson <saranderson13@gmail.com>
2019-03-19 16:09:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
   case
     when array.length == 1
-      only_element = array.pop
-      only_element
+      array.unshift
     when array.length == 2
       array.join(" and ")
     when array.length >= 3
